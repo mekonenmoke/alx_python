b808536851d625f52e3de54731f0f322c58fe822
mekonen <mekoninmoke@gmail.com>
2023-07-15 22:17:24 +0300

    Print

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+# This prints first three letters of word
+print("First 3 letters: {}".format(word[:3]))
+# This prints last two letters of word
+print("Last 2 letters: {}".format(word[-2:]))
+# This prints all middle letters of word
+print("Middle word: {}".format(word[1: -1]))
