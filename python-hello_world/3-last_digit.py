ca96c7bafbd4f0cb384a3e044fba71d3d29dce86
mekonen <mekoninmoke@gmail.com>
2023-07-15 23:20:43 +0300

    checke last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+# To ge last digit
+last_digit = abs(number) % 10
 '''
 This code is to check 
 1. the last number and
-1. Wether it is less than 6
-2. wether it is zero
+then for last number
+1. wether the last digit is greater than 5
+2. wether the last digit is 0
+3. wether the last digit is less than 6 and not 0
 '''
-last_digit = abs(number) % 10
 if (number < 0):
     last_digit = -last_digit
 if (last_digit > 5):
