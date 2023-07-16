9f97e85ca025d4bdda97673daa95f3e68bbe6890
mekonen <mekoninmoke@gmail.com>
2023-07-16 22:01:04 +0300

    print 00-99 in two digit

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,3 +1,3 @@
 # To print number 00 to 99 in two digit
 for num in range(100):
-    print(f"{num:02d}", end=", " if num != 99 else "\n")
+    print("{:02d}".format(num), end=", " if num != 99 else "\n")
