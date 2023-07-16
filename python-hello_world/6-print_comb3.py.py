1bf7eb7861c589f94d738c80819843cbd634b009
mekonen <mekoninmoke@gmail.com>
2023-07-16 22:04:37 +0300

    Print 0-89

--- python-hello_world/6-print_comb3.py.py
+++ python-hello_world/6-print_comb3.py.py
@@ -1,6 +1,6 @@
 for tens_digit in range(10):
     for ones_digit in range(tens_digit + 1, 10):
         print(
-            f"{tens_digit}{ones_digit}",
+            "{0}{1}".format(tens_digit, ones_digit),
             end=", " if tens_digit != 8 or ones_digit != 9 else "\n",
         )
