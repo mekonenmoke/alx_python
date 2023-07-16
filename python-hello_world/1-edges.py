88315bcc4df53718751892d6085db4ac62baa3b9
mekonen <mekoninmoke@gmail.com>
2023-07-16 21:31:07 +0300

    Print

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -5,4 +5,4 @@ print("First 3 letters: {}".format(word[:3]))
 # This prints last two letters of word
 print("Last 2 letters: {}".format(word[-2:]))
 # This prints all middle letters of word
-print("Middle word: {}".format(word[1: -1]))
+print("Middle word: {}".format(word[1:-1]))
