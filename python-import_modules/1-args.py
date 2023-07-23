fe6ee9476f2455d4e63149362f62e63b01852804
mekonen <mekoninmoke@gmail.com>
2023-07-23 22:30:18 +0300

    Print number of and list of arguments

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,18 @@
+import sys
+
+if __name__ == "__main__":
+    # Number of arguments
+    num_args = len(sys.argv) - 1
+
+    # Output the number of arguments
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num_args))
+
+    # Output the list of arguments
+    if num_args > 0:
+        for i in range(1, num_args + 1):
+            print("{}: {}".format(i, sys.argv[i]))
