7b3730af92f7a50725af4a516de0ed008eae68d6
mekonen <mekoninmoke@gmail.com>
2023-07-25 19:41:30 +0300

    Import add module

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+import add_0
+
+
 def main():
-    add = __import__("add_0").add
     a = 1
     b = 2
-    result = add(a, b)
+    result = add_0.add(a, b)
     print("{:d} + {:d} = {:d}".format(a, b, result))
 
 
