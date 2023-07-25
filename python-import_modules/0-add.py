d61668170c26d999982c2da7ca18222234134dfb
mekonen <mekoninmoke@gmail.com>
2023-07-25 19:20:31 +0300

    Import add module from add_0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    # declare two variable in different line
-    a = 1
-    b = 2
-    # import module add
-    add = __import__("add_0").add
-    # print the value
-    print("{} + {} = {}".format(a, b, add(a, b)))
+add = __import__("add_0").add
+a = 1
+b = 2
+result = add(a, b)
+print("{:d} + {:d} = {:d}".format(a, b, result))
