efe96c23c9d6913e9c7e3073f215cd069912dfc7
mekonen <mekoninmoke@gmail.com>
2023-07-25 19:39:08 +0300

    Import add module

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+def main():
     add = __import__("add_0").add
     a = 1
     b = 2
     result = add(a, b)
     print("{:d} + {:d} = {:d}".format(a, b, result))
+
+
+if __name__ == "__main__":
+    main()
