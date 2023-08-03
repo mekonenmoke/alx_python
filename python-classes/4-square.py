2491607ba9c66f1a775017ed4cf9e5572df1cdbc
mekonen <mekoninmoke@gmail.com>
2023-08-03 08:01:37 +0300

    Check if the object is exactly an instance

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,82 +1,22 @@
 """
-A class representing a square.
+A class for checking if an object is an instance of the specified class.
 """
 
 
-class Square:
+class TypeChecker:
     """
-    A class representing a square.
-
-    Attributes:
-        __size (int): The size of the square (private attribute).
+    A class for checking if an object is an instance of the specified class.
     """
 
-    def __init__(self, size=0):
+    def is_same_class(self, obj, a_class):
         """
-        Initializes a new Square instance.
+        Check if the object is exactly an instance of the specified class.
 
         Args:
-            size (int): The size of the square (default is 0).
-
-        Raises:
-            TypeError: If the provided size is not an integer.
-            ValueError: If the provided size is less than 0.
+            obj: The object to check.
+            a_class: The class to compare the object with.
 
         Returns:
-            None.
+            bool: True if the object is an instance of the specified class, False otherwise.
         """
-        self.size = size
-
-    @property
-    def size(self):
-        """
-        Get the size of the square.
-
-        Returns:
-            int: The size of the square.
-        """
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """
-        Set the size of the square.
-
-        Args:
-            value (int): The size of the square.
-
-        Raises:
-            TypeError: If the provided size is not an integer.
-            ValueError: If the provided size is less than 0.
-
-        Returns:
-            None.
-        """
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
-
-    def area(self):
-        """
-        Calculate the area of the square.
-
-        Returns:
-            int: The area of the square.
-        """
-        return self.__size**2
-
-    def my_print(self):
-        """
-        Print the square using '#' character.
-
-        Returns:
-            None.
-        """
-        if self.__size == 0:
-            print()
-        else:
-            for _ in range(self.__size):
-                print("#" * self.__size)
+        return type(obj) is a_class
