12270b02c8364a72c210068eca3f983a658fc6f8
mekonen <mekoninmoke@gmail.com>
2023-08-03 06:41:54 +0300

    class square whic contain private size attribute

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,8 @@
 class Square:
     def __init__(self, size):
+        """
+        This method is to initialize the class square
+
+        :param size: is the private variable
+        """
         self.__size = size
