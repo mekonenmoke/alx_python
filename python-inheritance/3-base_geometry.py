536f3ab23f1b58cc301ecf0afd9c860fdd6615ce
mekonen <mekoninmoke@gmail.com>
2023-08-04 06:51:01 +0300

    Base geometry

---
+++ python-inheritance/3-base_geometry.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""An empty base class for representing geometric entities."""
+
+
+class BaseGeometry:
+    """
+    An empty base class for representing geometric entities.
+
+    This class serves as a base class that other classes can inherit from to
+    define specific geometric entities and their behavior.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+
+    Example:
+        # Create an instance of the BaseGeometry class
+        bg = BaseGeometry()
+
+        # Print the object representation and attributes of the instance
+        print(bg)
+        print(dir(bg))
+
+    """
+
+    pass
