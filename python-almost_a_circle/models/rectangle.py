63ac08a296031319298b430cef2624aa1ab3d0fb
mekonen <mekoninmoke@gmail.com>
2023-08-09 12:35:30 +0300

    Rectangle class inherited from Base

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+"""
+    Creating the base class of all other classes for this project.
+"""
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    Rectangle class representing a rectangle object.
+
+    Attributes:
+        width (int): Width of the rectangle.
+        height (int): Height of the rectangle.
+        x (int): X-coordinate position of the rectangle.
+        y (int): Y-coordinate position of the rectangle.
+        id (int): ID of the rectangle object.
+
+    Methods:
+        __init__(self, width, height, x=0, y=0, id=None): Constructor method to initialize the rectangle.
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initialize a new Rectangle object.
+
+        Args:
+            width (int): Width of the rectangle.
+            height (int): Height of the rectangle.
+            x (int, optional): X-coordinate position of the rectangle. Defaults to 0.
+            y (int, optional): Y-coordinate position of the rectangle. Defaults to 0.
+            id (int, optional): ID of the rectangle object. Defaults to None.
+
+        Returns:
+            None
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Getter method for the width attribute."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter method for the width attribute."""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter method for the height attribute."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter method for the height attribute."""
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter method for the x attribute."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter method for the x attribute."""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter method for the y attribute."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter method for the y attribute."""
+        self.__y = value
