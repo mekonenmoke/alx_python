a401dd6bfc626dac7914acb0f7c3a5eaee920ac9
mekonen <mekoninmoke@gmail.com>
2023-08-09 12:06:01 +0300

    python unit test

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,30 @@
+class Base:
+    """
+    Base class representing a base object.
+
+    Attributes:
+        __nb_objects (int): Private class attribute to keep track of the number of instances.
+        id (int): Public instance attribute representing the object's ID.
+
+    Methods:
+        __init__(self, id=None): Constructor method to initialize the object.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initialize a new Base object.
+
+        Args:
+            id (int, optional): If provided, the ID to assign to the object. Defaults to None.
+                               If not provided, auto-increments __nb_objects and assigns the new value to the ID.
+
+        Returns:
+            None
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
