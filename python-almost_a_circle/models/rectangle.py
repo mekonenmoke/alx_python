efff1412d06884bff41f4d5ad2d183dce59025a7
mekonen <mekoninmoke@gmail.com>
2023-08-09 13:34:25 +0300

    Update Rectangle class

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -111,3 +111,17 @@ class Rectangle(Base):
     def __str__(self):
         """Return a string representation of the rectangle."""
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+
+    def update(self, *args):
+        """Update attributes using variable arguments."""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
