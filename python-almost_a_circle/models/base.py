5916cc4d0cb440cb1cf6b814da86acba486a1166
mekonen <mekoninmoke@gmail.com>
2023-08-09 12:28:03 +0300

    almost a circle

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -6,14 +6,29 @@
 
 class Base:
     """
-    This class will manage the id attribute for all the classes.
-    Arguments:
-        @id: The id for a specific instance.
+    Base class representing a base object.
+
+    Attributes:
+        __nb_objects (int): Private class attribute to keep track of the number of instances.
+        id (int): Public instance attribute representing the object's ID.
+
+    Methods:
+        __init__(self, id=None): Constructor method to initialize the object.
     """
 
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        Initialize a new Base object.
+
+        Args:
+            id (int, optional): If provided, the ID to assign to the object. Defaults to None.
+                               If not provided, auto-increments __nb_objects and assigns the new value to the ID.
+
+        Returns:
+            None
+        """
         if id is not None:
             self.id = id
         else:
