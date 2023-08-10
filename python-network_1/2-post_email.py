3100ee1115f9c58361517cd1c5f4954139fe3ad4
mekonen <mekoninmoke@gmail.com>
2023-08-10 22:06:48 +0300

    Sends a POST request to the passed URL

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """
-Script that takes in a URL and an email, sends a POST request to the passed
-URL with the email as a parameter, and displays the body of the response
-(decoded in utf-8).
+Sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
 """
-from sys import argv
-from urllib.parse import urlencode
-from urllib.request import Request, urlopen
-
 
 if __name__ == "__main__":
-    url = argv[1]
-    value = {"email": argv[2]}
-    data = urlencode(value).encode("ascii")
-    req = Request(url, data)
+    import requests
+    import sys
+    from urllib.parse import urlencode
 
-    with urlopen(req) as response:
-        print(response.read().decode("utf-8", "replace"))
+    argv = sys.argv
+    url = argv[1]
+    email = argv[2]
+    data = urlencode({"email": email})
+    data = data.encode("ascii")
+
+    response = requests.post(url, data=data)
+    print("Your email is:", response.text)
