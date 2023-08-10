7c1c047d813c64e3b3da17e82eec76d8305948ff
mekonen <mekoninmoke@gmail.com>
2023-08-10 08:54:33 +0300

    fetches https://intranet.hbtn.io/status.

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,14 +2,29 @@
 """A script that
 fetches https://intranet.hbtn.io/status.
 """
+import requests
+
+
+def fetch_and_display_status(url):
+    """
+    Fetches the content from a given URL and displays information about the response.
+
+    Args:
+        url (str): The URL to fetch the content from.
+
+    Returns:
+        None
+    """
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        print("Body response:")
+        print("\t- type:", type(response.text))
+        print("\t- content:", response.text.strip())
+    else:
+        print("Failed to fetch the URL. Status code:", response.status_code)
 
 
 if __name__ == "__main__":
-    import urllib.request
-
-    with urllib.request.urlopen("https://intranet.hbtn.io/status") as resp:
-        content = resp.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
+    target_url = "https://alu-intranet.hbtn.io/status"
+    fetch_and_display_status(target_url)
