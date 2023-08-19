2953055e83b7c8df8de13120c7f41f4a8a84ec58
mekonen <mekoninmoke@gmail.com>
2023-08-19 14:30:11 +0300

    List all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,29 +2,46 @@
 """
 import MySQLdb
 
-username = "root"
-password = "0735"
-name = "hbtn_0e_0_usa"
-# Connect to the MySQL server
-connection = MySQLdb.connect(
-    host="localhost", port=3306, user=username, password=password, db=name
-)
 
-# Create a cursor object
-cursor = connection.cursor()
+def list_states(username, password, db_name):
+    """
+    Retrieve and display a list of states from a MySQL database.
 
-# Execute the query
-cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    Args:
+        username (str): MySQL username.
+        password (str): MySQL password.
+        db_name (str): Name of the database.
 
-# Fetch all rows
-results = cursor.fetchall()
+    Returns:
+        None
+    """
 
-# Print the results
-for row in results:
-    print(row)
+    # Connect to the MySQL server
+    connection = MySQLdb.connect(
+        host="localhost", port=3306, user=username, password=password, db=db_name
+    )
 
-# Close the cursor object
-cursor.close()
+    # Create a cursor to execute queries
+    cursor = connection.cursor()
 
-# Close the connection
-connection.close()
+    # Execute the query to retrieve states
+    query = "SELECT * FROM states ORDER BY id ASC"
+    cursor.execute(query)
+
+    # Fetch all results
+    results = cursor.fetchall()
+
+    # Display results
+    for row in results:
+        print(row)
+
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
+
+
+if __name__ == "__main__":
+    username = "root"
+    password = "0735"
+    db_name = "hbtn_0e_0_usa"
+    list_states(username, password, db_name)
