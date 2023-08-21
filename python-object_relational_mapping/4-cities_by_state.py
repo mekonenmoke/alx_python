366804112bd7e9ddf5fad4144976550bede946f9
mekonen <mekoninmoke@gmail.com>
2023-08-21 20:32:01 +0300

    select all cities in the states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     )
     cur = db.cursor()
     cur.execute(
-        """SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON states.id == cities.id ORDER BY cities.id ASC"""
+        """SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON states.id = cities.id ORDER BY cities.id ASC"""
     )
     rows = cur.fetchall()
     for row in rows:
