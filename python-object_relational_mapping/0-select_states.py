e33a11305ac80fae360bf815aa40d5da55f7698a
mekonen <mekoninmoke@gmail.com>
2023-08-21 19:43:45 +0300

    List all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,48 +1,21 @@
-"""a script that lists all states from the database
-"""
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
-
-
-def list_states(username1, password1, db_name1):
-    """
-    Retrieve and display a list of states from a MySQL database.
-
-    Args:
-        username1 (str): MySQL username.
-        password1 (str): MySQL password.
-        db_name1 (str): Name of the database.
-
-    Returns:
-        None
-    """
-
-    # Connect to the MySQL server
-    connection = MySQLdb.connect(
-        host="localhost", port=3306, user=username1, password=password1, db=db_name1
-    )
-
-    # Create a cursor to execute queries
-    cursor = connection.cursor()
-
-    # Execute the query to retrieve states
-    query = """SELECT * FROM states ORDER BY id ASC"""
-    cursor.execute(query)
-
-    # Fetch all results
-    results = cursor.fetchall()
-
-    # Display results
-    for row in results:
-        print(row)
-
-    # Close the cursor and connection
-    cursor.close()
-    connection.close()
+import sys
 
 
 if __name__ == "__main__":
-    username = "root"
-    password = "0735"
-    db_name = "hbtn_0e_0_usa"
-
-    list_states(username, password, db_name)
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306,
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
