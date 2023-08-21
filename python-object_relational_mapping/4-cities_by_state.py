e12202b4c1f11c9ea3a7aca709741929e324dde5
mekonen <mekoninmoke@gmail.com>
2023-08-21 20:27:14 +0300

    List all cities in states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,9 @@ if __name__ == "__main__":
         "localhost", user=sys.argv[1], password=sys.argv[2], db=sys.argv[3], port=3306
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+    cur.execute(
+        """SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON states.id == cities.id ORDER BY cities.id ASC"""
+    )
     rows = cur.fetchall()
     for row in rows:
         print(row)
