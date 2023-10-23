5b44bc911b360efc367847d2cef1936d1c28eef4
mekonenmoke <mekoninmoke@gmail.com>
2023-10-23 00:30:58 -0900

    gather data

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -25,7 +25,7 @@ def get_employee_info(employee_id):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
+        print("Usage: python 0-gather_data_from_an_API.py <employee_id>")
     else:
         try:
             employee_id = int(sys.argv[1])
