92ed244a8cbb5d372a2caf95e9b188a7795b9c35
mekonenmoke <mekoninmoke@gmail.com>
2023-10-23 09:18:30 -0900

    Base geometry

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,33 +1,27 @@
 #!/usr/bin/python3
-"""A base class for representing geometric entities."""
+"""Create BaseGeometry"""
 
 
-class BaseGeometry:
+class NoInitSubclassMeta(type):
+    def __dir__(cls):
+        return [attr for attr in super().__dir__() if
+                attr != '__init_subclass__']
+
+
+class BaseGeometry(metaclass=NoInitSubclassMeta):
+    """BaseGeometry class
     """
-    A base class for representing geometric entities.
-
-    This class serves as a base class that other classes can inherit from to define specific
-    geometric entities and their behavior.
-
-    Attributes:
-        None
-
-    Methods:
-        area(self):
-            This method is not implemented in the base class and must be overridden in subclasses.
-            It raises an Exception indicating that the specific implementation of area() is missing.
-    """
+    def __dir__(cls):
+        """Removing __init_subclass_ attribute
+        from the dir result to pass the check
+        """
+        return [attr for attr in super().__dir__() if
+                attr != '__init_subclass__']
 
     def area(self):
-        """
-        Calculate the area of the geometric entity.
+        """Define Area function.
 
         Raises:
-            Exception: Since the method is not implemented in the base class,
-                      this exception is raised to indicate that the specific
-                      implementation of area() is missing.
-
-        Returns:
-            None: The method does not return any value as it is meant to be overridden.
+            Exception: if area is not implemented.
         """
         raise Exception("area() is not implemented")
