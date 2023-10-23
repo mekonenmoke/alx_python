470a6d399df1d64615c04e6c8c00ef91a8e88d14
mekonenmoke <mekoninmoke@gmail.com>
2023-10-23 01:19:36 -0900

    export

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -2,33 +2,39 @@ import csv
 import requests
 import sys
 
-def get_employee_todo_progress(employee_id):
+def get_employee_info(employee_id):
     # Get employee details
-    employee_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
     response = requests.get(employee_url)
     employee_data = response.json()
-    user_id = str(employee_data["id"])  # Convert USER_ID to string
-    username = employee_data["username"]
+    user_id = employee_data['id']
+    username = employee_data['username']
 
-    # Get employee TODO list
-    todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
-    response = requests.get(todo_url)
+    # Get employee's TODO list
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    response = requests.get(todos_url)
     todos = response.json()
 
-    # Export data to CSV
-    csv_file_name = "{}.csv".format(user_id)
-    with open(csv_file_name, mode='w', newline='') as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        for todo in todos:
-            task_completed_status = todo["completed"]  # TASK_COMPLETED_STATUS as boolean
-            task_title = todo["title"]
-            writer.writerow([user_id, username, task_completed_status, task_title])
+    # Prepare data for CSV
+    csv_data = []
+    for todo in todos:
+        task_completed_status = "True" if todo['completed'] else "False"
+        task_title = todo['title']
+        csv_data.append([user_id, username, task_completed_status, task_title])
 
-    print("Data exported to: {}".format(csv_file_name))
+    # Write data to CSV file
+    with open(f"{user_id}.csv", "w", newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerows(csv_data)
+
+    print(f"Data exported to {user_id}.csv")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python script.py <employee_id>")
     else:
-        employee_id = int(sys.argv[1])
-        get_employee_todo_progress(employee_id)
+        try:
+            employee_id = int(sys.argv[1])
+            get_employee_info(employee_id)
+        except ValueError:
+            print("Invalid employee ID. Please enter a valid integer.")
