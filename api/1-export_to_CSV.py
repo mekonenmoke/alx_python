de84b76ea7c5564fb190978458e51e39f7b3a9c9
mekonenmoke <mekoninmoke@gmail.com>
2023-10-23 01:07:18 -0900

    Export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -2,40 +2,34 @@ import requests
 import csv
 import sys
 
-def get_employee_info(employee_id):
+def get_employee_todo_progress(employee_id):
     # Get employee details
-    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    employee_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
     response = requests.get(employee_url)
     employee_data = response.json()
-    user_id = employee_data['id']
-    username = employee_data['username']
+    user_id = str(employee_data["id"])  # Convert USER_ID to string
+    username = employee_data["username"]
 
-    # Get employee's TODO list
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-    response = requests.get(todos_url)
+    # Get employee TODO list
+    todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
+    response = requests.get(todo_url)
     todos = response.json()
 
-    # Prepare data for CSV
-    csv_data = []
-    for todo in todos:
-        task_completed_status = "Completed" if todo['completed'] else "Not Completed"
-        task_title = todo['title']
-        csv_data.append([user_id, username, task_completed_status, task_title])
+    # Export data to CSV
+    csv_file_name = "{}.csv".format(user_id)
+    with open(csv_file_name, mode='w', newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        writer.writerow(['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE'])
+        for todo in todos:
+            task_completed_status = todo["completed"]  # TASK_COMPLETED_STATUS as boolean
+            task_title = todo["title"]
+            writer.writerow([user_id, username, task_completed_status, task_title])
 
-    # Write data to CSV file
-    with open(f"{user_id}.csv", "w", newline='') as csvfile:
-        csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        csv_writer.writerows(csv_data)
-
-    print(f"Data exported to {user_id}.csv")
+    print("Data exported to: {}".format(csv_file_name))
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python script.py <employee_id>")
     else:
-        try:
-            employee_id = int(sys.argv[1])
-            get_employee_info(employee_id)
-        except ValueError:
-            print("Invalid employee ID. Please enter a valid integer.")
+        employee_id = int(sys.argv[1])
+        get_employee_todo_progress(employee_id)
